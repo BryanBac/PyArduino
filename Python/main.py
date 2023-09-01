5c5c2dc122fdecdb0c074f8f4a63bf60250cb1f3
Diego090200 <alehernandezhernandez09@gmail.com>
2023-09-01 11:26:53 -0600

    Prueba Tiempo

--- Python/main.py
+++ Python/main.py
@@ -1,7 +1,9 @@
 import serial 
-import time 
+import datetime
+from tiempos import obtener_hora, obtener_fecha, tiempo_transcurrido
 
 arduino = serial.Serial(port='COM3', baudrate=9600, timeout=0.1)
+hora_parametro = datetime.datetime.now().time()
 
 def obtener():
     data = arduino.readline()
@@ -9,3 +11,10 @@ def obtener():
 
 while True:
     obtener()
+    hora_actual = obtener_hora()
+    fecha_actual = obtener_fecha()
+    print("Hora actual:", hora_actual)
+    print("Fecha actual:", fecha_actual)
+    if tiempo_transcurrido(hora_parametro):
+        print("Ha transcurrido un minuto.")
+        hora_parametro = datetime.datetime.now().time()
